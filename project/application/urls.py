262008e5cd4b526941372bcb49b7b65698a7ceb2
martin-ngigi <martinwainaina001@gmail.com>
2022-12-06 21:57:41 +0300

    Added Postman collection

--- project/application/urls.py
+++ project/application/urls.py
@@ -6,9 +6,9 @@ from rest_framework.routers import DefaultRouter
 			
 
 router = DefaultRouter()
-router.register(r'add-multiple', BookViewSet, basename='user')
+router.register(r'add-multiple', BookViewSet, basename='user') #http://127.0.0.1:8000/books/add-multiple/
 #urlpatterns = router.urls
 
 urlpatterns =[
-    path('add/', BookList.as_view(), name='books'),
+    path('add/', BookList.as_view(), name='books'), #http://127.0.0.1:8000/books/add/
 ] + router.urls
