8ff177fd916c496dca5f2b098e9c96d4601e67aa
martin-ngigi <martinwainaina001@gmail.com>
2022-12-06 21:54:06 +0300

    Posting a single and a list using same ViewSet Success

--- project/application/urls.py
+++ project/application/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import BookList
 
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+			
+
+router = DefaultRouter()
+router.register(r'add-multiple', BookViewSet, basename='user')
+#urlpatterns = router.urls
+
 urlpatterns =[
     path('add/', BookList.as_view(), name='books'),
-]
+] + router.urls
--- project/application/views.py
+++ project/application/views.py
@@ -3,7 +3,52 @@ from django.shortcuts import render
 from .models import Books
 from .serializers import BookSerializer
 from rest_framework.response import Response
-from rest_framework import status, mixins, generics
+from rest_framework import status, mixins, generics, viewsets
+
+
+
+'''
+Can creata a list of books and a single book as well ;-)
+Also accepts dict {} for a single object and a list/array [] for multiple objects
+'''
+class BookViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    ViewSet create and list books
+
+    Usage single : POST
+    {
+        "name":"Killing Floor: A Jack Reacher Novel", 
+        "author":"Lee Child"
+    }
+
+    Usage array : POST
+    [{  
+        "name":"Mr. Mercedes: A Novel (The Bill Hodges Trilogy)",
+        "author":"Stephen King"
+    },{
+        "name":"Killing Floor: A Jack Reacher Novel", 
+                "author":"Lee Child"
+    }]
+    """
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+    search_fields = ('name','author')
+
+    def create(self, request, *args, **kwargs):
+        """
+        #checks if post request data is an array initializes serializer with many=True
+        else executes default CreateModelMixin.create function 
+        """
+        is_many = isinstance(request.data, list)
+        if not is_many:
+            return super(BookViewSet, self).create(request, *args, **kwargs)
+        else:
+            serializer = self.get_serializer(data=request.data, many=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+			
 
 
 # POSTING LIST/ARRAY i.e []
